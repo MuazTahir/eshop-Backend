0638bc6b0a3148e5449397aadddc775f43e74693
MuazTahir <muaztahir342@gmail.com>
2025-05-06 22:10:10 +0500

    miner mistake

--- app/server.js
+++ app/server.js
@@ -33,7 +33,7 @@ app.get('/api/ping', (req, res) => {
 app.use(
     cors(
         {
-            origin: ['CLIENT_URL', 'http://192.168.100.230:3000'],
+            origin: [process.env.CLIENT_URL, 'http://192.168.100.230:3000'],
             methods: ['GET', 'POST', 'DELETE', 'PUT'],
             allowedHeaders: [
                 'Content-Type',
